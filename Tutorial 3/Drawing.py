6ea88c229ecef2558127c9d350463b42fd978403
AVI18794 <lakshyatripathi94@gmail.com>
2020-11-07 12:02:40 +0530

    Codes added for drawing rectangle

--- Tutorial 3/Drawing.py
+++ Tutorial 3/Drawing.py
@@ -21,5 +21,19 @@ cv2.imshow('Canvas',canvas)
 red = (0,0,255)
 cv2.line(canvas,(300,0),(0,300),red,3)
 cv2.imshow('Canvs with red line',canvas)
+
+#Draw a green 60 x 60 pixel square , starting at 10,10 and ending at 60,60
+cv2.rectangle(canvas,(10,10),(60,60),green)
+cv2.imshow('Canvas having square',canvas)
+
+#Draw another rectangle
+cv2.rectangle(canvas,(50,200),(200,225),red,5)
+cv2.imshow('Thick Rectangle of Red Color',canvas)
+
+#Draw a solid rectangle , to draw a solid rectangle pass the argument as -1
+blue = (255,0,0)
+cv2.rectangle(canvas,(200,50),(225,125),blue,-1)
+cv2.imshow('Canvas with filled blue rectangle',canvas)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
